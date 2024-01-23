9368c14c9590ca14f8b18366b4d8b64468415d89
Nwaneri Uchechukwu Chigozie <uchenwaneri27@gmail.com>
2024-01-23 18:21:06 +0100

    another fix. removed title and links from document to app.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,22 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>ClearLink-Uchechukwu</title>
-        <link rel="shortcut icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/apple-icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
